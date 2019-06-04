1b27665abaa66eb0a51068f4dd4b660984061878
Rico Yu <ricoyu520@gmail.com>
2019-06-04 11:04:28 +0800

    升级到4.6.1版本

--- loser-cache/src/main/java/com/loserico/cache/redis/redisson/config/SimgleNodeRedissonConfig.java
+++ loser-cache/src/main/java/com/loserico/cache/redis/redisson/config/SimgleNodeRedissonConfig.java
@@ -1,7 +1,6 @@
 package com.loserico.cache.redis.redisson.config;
 
 import org.redisson.config.Config;
-import org.redisson.config.TransportMode;
 
 public class SimgleNodeRedissonConfig implements RedissonConfigurable {
 
@@ -18,7 +17,7 @@ public class SimgleNodeRedissonConfig implements RedissonConfigurable {
 		 * useReplicatedServers 	– for replicated nodes. 
 		 * @on
 		 */
-		config.setTransportMode(TransportMode.EPOLL);
+//		config.setTransportMode(TransportMode.EPOLL);
 		config.useSingleServer()
 				//Redisson 3.7.0 采用redis://前缀的，老版本不带redis://前缀
 				.setAddress(redissonProperties.getAddress())
